3163bf064f0ba71e1b106806ac51aec39e7d8b52
eradaali <erada.abdalrhman@gmail.com>
2021-05-19 02:36:51 +0300

    ch2

--- app.js
+++ app.js
@@ -4,28 +4,28 @@ let numAttempt = 0;
 let maxNumAttempts = 25;
 let attemptsElement = document.getElementById('attempt');
 let arrayImg = [];
-let leftimg =document.getElementById('leftImg');
-let centerimg=document.getElementById('centerImg');
-let rightimg =document.getElementById('rightImg');
-let lindex =0;
+let leftimg = document.getElementById('leftImg');
+let centerimg = document.getElementById('centerImg');
+let rightimg = document.getElementById('rightImg');
+let lindex = 0;
 let cindex;
 let rindex;
 let goatsClicks = [];
 let goatsViews = [];
 
 let goatsImagesNames = [];
-function getImage (imgName){
+function getImage(imgName) {
     this.imgName = imgName.split('.')[0];
-    this.sourse='img/' + imgName;
-    this.view=0;
-    this.click=0;
+    this.sourse = 'img/' + imgName;
+    this.view = 0;
+    this.click = 0;
     arrayImg.push(this);
     goatsImagesNames.push(this.goatName);
     //getImage.arrayImg.push(this);
     settingItems();
 
 }
-let imgarray=['/bag.jpg','/banana.jpg','/bathroom.jpg','/boots.jpg','/breakfast.jpg','/bubblegum.jpg','/chair.jpg','/cthulhu.jpg','/dog-duck.jpg','/dragon.jpg','/pen.jpg','/pet-sweep.jpg','/scissors.jpg','/shark.jpg','/sweep.png','/tauntaun.jpg','/unicorn.jpg','/water-can.jpg','/wine-glass.jpg'];
+let imgarray = ['/bag.jpg', '/banana.jpg', '/bathroom.jpg', '/boots.jpg', '/breakfast.jpg', '/bubblegum.jpg', '/chair.jpg', '/cthulhu.jpg', '/dog-duck.jpg', '/dragon.jpg', '/pen.jpg', '/pet-sweep.jpg', '/scissors.jpg', '/shark.jpg', '/sweep.png', '/tauntaun.jpg', '/unicorn.jpg', '/water-can.jpg', '/wine-glass.jpg'];
 
 for (let i = 0; i < imgarray.length; i++) {
     new getImage(imgarray[i]);
@@ -38,7 +38,7 @@ function settingItems() {
 }
 function gettingItems() {
     let stringObj = localStorage.getItem('coffee');
-    
+
     let normalObj = JSON.parse(stringObj);
 
     if (normalObj !== null) {
@@ -47,29 +47,36 @@ function gettingItems() {
     renderImg();
 }
 
+let perviousarray = [];
 
-function myimg (){
-    return Math.floor(Math.random() * arrayImg.length);
+function myimg() {
+let  randomNumber ;
+    while (true) {
+        randomNumber = Math.floor(Math.random() * arrayImg.length);
+        if (perviousarray.indexOf(randomNumber) < 0)
+            return randomNumber;
+
+    }
+
 }
 
-let perviousarray=[];
 
-function addKey(key,value){
+function addKey(key, value) {
     debugger
-    localStorage.setItem(key,JSON.stringify(value));
+    localStorage.setItem(key, JSON.stringify(value));
 }
-function getKey(key){
-    
- return   localStorage.getItem(key);
+function getKey(key) {
+
+    return localStorage.getItem(key);
 }
 function renderImg() {
     lindex = myimg();
-    rindex=myimg();
-    cindex=myimg();
-    while (lindex === rindex || lindex === cindex || rindex === cindex){
-    lindex=myimg();
-    rindex=myimg();
-    
+    rindex = myimg();
+    cindex = myimg();
+    while (lindex === rindex || lindex === cindex || rindex === cindex) {
+        lindex = myimg();
+        rindex = myimg();
+
     }
     console.log(lindex);
     leftimg.setAttribute('src', arrayImg[lindex].sourse);
@@ -87,6 +94,9 @@ function renderImg() {
 
     attemptsElement.textContent = attempt;
     console.log('left', lindex);
+    perviousarray=[];    perviousarray.push(lindex)
+    perviousarray.push(rindex)
+    perviousarray.push(cindex)
 
 }
 
@@ -100,45 +110,46 @@ rightimg.addEventListener('click', clicks);
 
 function clicks(event) {
     numAttempt++;
-    if(numAttempt <=maxNumAttempts){
+    if (numAttempt <= maxNumAttempts) {
         console.log(event.target.id)
-    
-    if(event.target.id === 'leftImg'){
-        arrayImg[lindex].click++;
+
+        if (event.target.id === 'leftImg') {
+            arrayImg[lindex].click++;
+        }
+        else if (event.target.id === 'centerImg') {
+            arrayImg[cindex].click++;
+        }
+        else if (event.target.id === 'rightImg') {
+            arrayImg[rindex].click++;
+        }
     }
-    else if(event.target.id === 'centerImg'){
-        arrayImg[cindex].click++;
+    else {
+
+        let btn = document.getElementById('btn');
+        let ulelement = document.getElementById('results');
+        btn.addEventListener('click', result);
+        let lielement;
+        function result() {
+            for (let i = 0; i < arrayImg.length; i++) {
+
+                lielement = document.createElement('li');
+                ulelement.appendChild(lielement);
+                lielement.textContent = `${arrayImg[i].imgName} has ${arrayImg[i].view} view and has ${arrayImg[i].click} clicks.`
+
+            }
+            addKey("products", arrayImg)
+            alert(getKey("products"))
+
+            chartRender();
+        }
+
+
+        leftimg.removeEventListener('click', clicks);
+        rightimg.removeEventListener('click', clicks);
+        centerimg.removeEventListener('click', clicks);
+
     }
-    else if(event.target.id === 'rightImg'){
-        arrayImg[rindex].click++;
-    }}
-else {
-    
-let btn=document.getElementById('btn');
-   let ulelement =document.getElementById('results');
-   btn.addEventListener('click',result);
-   let lielement;
-   function result() {
-    for (let i=0;i<arrayImg.length;i++){
-        
-        lielement=document.createElement('li');
-        ulelement.appendChild(lielement);
-        lielement.textContent= `${arrayImg[i].imgName} has ${arrayImg[i].view} view and has ${arrayImg[i].click} clicks.`
-       
-    }
-    addKey("products",arrayImg)
-    alert(getKey("products"))
-
-    chartRender();
-   }
-
-
-leftimg.removeEventListener('click', clicks);
-rightimg.removeEventListener('click', clicks);
-centerimg.removeEventListener('click', clicks);
-
-}
-renderImg();
+    renderImg();
 
 }
 
@@ -150,9 +161,10 @@ renderImg();
 console.log(goatsViews);
 console.log(goatsClicks);
 function chartRender() {
-    for(let i=0;i<arrayImg.length;i++){
-    goatsClicks.push(arrayImg[i].click);
-     goatsViews.push(arrayImg[i].view);}
+    for (let i = 0; i < arrayImg.length; i++) {
+        goatsClicks.push(arrayImg[i].click);
+        goatsViews.push(arrayImg[i].view);
+    }
     var ctx = document.getElementById('myChart').getContext('2d');
     var myChart = new Chart(ctx, {
         type: 'bar',
